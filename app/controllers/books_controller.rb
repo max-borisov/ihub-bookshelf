5a7dcdab890676a75e4d97d44c4599615bfc4483
max-borisov <max.borisov@yahoo.com>
2015-06-17 11:30:29 +0300

    up edit/new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,53 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   # List books
   def index
     @books = Book.order(created_at: :asc).all
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
   # Show book
   def show
-    @book = Book.find(params[:id])
     @reviews = @book.reviews
   end
+  
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated'
+    else
+      render :edit
+    end
+  end
+    
+  def destroy
+    title = @book.title
+    @book.destroy
+    redirect_to books_path, notice: "Book \"#{title}\" has been deleted"
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :publisher, :pub_date, :description, :price, :isbn, :amazon_id)
+    end
 end
