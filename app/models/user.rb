69477ba50e9d639f1bb3c253abead1a6bdd8ce63
max-borisov <max.borisov@yahoo.com>
2015-06-19 14:49:04 +0300

    up front page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy
   has_many :shopping_cart_items, dependent: :destroy
+  has_many :books, through: :shopping_cart_items
   validates :name, presence: true
 
   def has_in_shopping_cart?(book)
     shopping_cart_items.where(book_id: book).exists?
+  end
+
+  def shopping_cart_total_price
+    books.pluck(:price).sum
   end
 end
