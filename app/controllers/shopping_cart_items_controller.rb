cb2f89bcab549d975c8887344266fba4c07ce7bf
max-borisov <max.borisov@yahoo.com>
2015-06-23 17:28:45 +0300

    up add book btn

--- app/controllers/shopping_cart_items_controller.rb
+++ app/controllers/shopping_cart_items_controller.rb
@@ -7,9 +7,9 @@ class ShoppingCartItemsController < ApplicationController
     shopping_cart_item = current_user.shopping_cart_items.build(book_id: params[:book_id])
     book_title = Book.find(params[:book_id]).title
     if shopping_cart_item.save
-      redirect_to root_url, notice: "\"#{book_title}\" book has been added to your shopping cart"
+      redirect_to :back, notice: "\"#{book_title}\" book has been added to your shopping cart"
     else
-      redirect_to root_url, danger: "\"#{book_title}\" could not be added to your shopping cart. Please, try again."
+      redirect_to :back, danger: "\"#{book_title}\" could not be added to your shopping cart. Please, try again."
     end
   end
 
