62edd0751322f7e8294d602946c8f5834a92853f
max-borisov <max.borisov@yahoo.com>
2015-06-25 12:11:53 +0300

    up assets, up md

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # resources :users, only: [:show]
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
   resources :users, only: [:show, :destroy]
-
   root 'books#index'  
-  resources :books do
-    # resources :reviews
-    # resources :reviews, except: [:edit, :update], on: :member
+  resources :books do    
     resources :reviews, on: :member
   end
   resources :shopping_cart_items, only: [:index, :create, :destroy]
